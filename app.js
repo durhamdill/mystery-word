e1610d002711c637a476c0ab4595e8c50320c959
Katy Dillard <durhamdill@gmail.com>
2017-08-27 11:27:45 -0400

    add/fix double guess check

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mustache = require('mustache-express');
 const session = require('express-session');
+const validator = require('validator');
+const expressValidator = require('express-validator');
 const port = 3000;
 
 // INITIATE APP:
@@ -27,16 +29,17 @@ app.engine('mustache', mustache());
 app.set('views', './views');
 app.set('view engine', 'mustache');
 
-// BODY PARSER PARTICULARS:
+// BODY PARSER/VALIDATOR PARTICULARS:
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(expressValidator());
 
 // GRANT ACCESS TO STATIC FILES:
 app.use(express.static(__dirname + '/public'));
 
 
-
+//GET-POST/ROUTE SETUP:
 app.get('/', function(req, res){
   randomWord(req, res);
   console.log("Mystery Word: " + req.session.word);
@@ -45,14 +48,13 @@ app.get('/', function(req, res){
 })
 
 app.post('/', function(req, res){
-  checkGuess(req, res);
-  statusUpdate(req, res);
+  validateGuess(req, res);
+  // compareGuess(req, res);
+  // statusUpdate(req, res);
   res.render('game', {word:req.session.word, letters: req.session.wordArray, guesses:req.session.guesses, lives: req.session.lives, message: req.session.message});
 })
 
-
-
-//Generate random word and matching array with blank spaces:
+//GENERATE RANDOM WORD AND MATCHING ARRAY WITH BLANKS:
 function randomWord(req, res) {
   let word = dictionary[Math.floor(Math.random() * dictionary.length)];
   req.session.word = word;
@@ -65,7 +67,22 @@ function randomWord(req, res) {
   // console.log(req.session.wordArray);
 }
 
-function checkGuess(req, res) {
+function validateGuess(req, res) {
+  // console.log(req.session.guesses);
+  if (req.session.guesses.includes(req.body.letter.toUpperCase())){
+    console.log("not valid: double guess");
+    req.session.message = "You already tried that one. Try again!";
+  } else if (validator.isAlpha(req.body.letter) && validator.isLength(req.body.letter, { min: 1, max: 1})){
+      console.log("valid");
+      compareGuess(req, res);
+  } else {
+    console.log("not valid: invalid input");
+    req.session.message = "Invalid guess. Please choose one letter from the alphabet.";
+  }
+}
+
+//COMPARE GUESS AGAINST MYSTERY WORD:
+function compareGuess(req, res) {
   req.session.matches = 0;
   let guess = req.body.letter.toLowerCase();
   let word = req.session.word;
@@ -82,8 +99,10 @@ function checkGuess(req, res) {
     }
   }
   console.log("Total matches: " + req.session.matches);
+  statusUpdate(req, res);
 }
 
+//SEND STATUS UPDATE BACK TO PLAYER:
 function statusUpdate(req, res) {
   // console.log("hi");
   // console.log(req.session.matches);
@@ -96,10 +115,8 @@ function statusUpdate(req, res) {
   }
 }
 
-function validateGuess(req, res) {
-  
-}
 
+//CONNECT TO SERVER:
 app.listen(port, function(req, res){
    console.log('Starting mystery word game...');
   });
